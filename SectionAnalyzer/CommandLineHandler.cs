66fc968e7928f3b378cd116e350b4e37fdb06e38
sergbelom <SergBelom@yandex.ru>
2020-02-16 23:08:25 +0300

    Added command line handler.

---
+++ SectionAnalyzer/CommandLineHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace SectionAnalyzer
+{
+    public class Options
+    {
+        /// <summary>
+        /// Property for input files collection.
+        /// </summary>
+        [Option('f', "file", Required = true, HelpText = "Path to txt file with coordinates of triangles.")]
+        public String PathToFile { get; set; }
+    }
+}
